485fada490a897bff7890dd571e21458bc066bdd
Paul-Ladyman <ladyman.paul@gmail.com>
2017-09-24 19:27:49 +0100

    Abstract operations. Simplify looping logic.

--- attinytest.ino
+++ attinytest.ino
@@ -1,46 +1,61 @@
+#define PWMHIGH 255
+#define PWMLOW 0
+#define ANALOGLOW 0
+#define ANALOGHIGH 1023
+
 int pwmPin = 0;
 int pwmPin2 = 1;
 int analogInPin = A1;
-int i = 0;
-int stage = 0; // fade in
 
-void setup()
-{
+int i = 0;
+boolean fadingIn = true;
+
+void setup() {
   pinMode(pwmPin, OUTPUT);
   pinMode(pwmPin2, OUTPUT);
   pinMode(analogInPin, INPUT);
 }
 
-void loop()
-{
+int readTimerInput(void) {
   int timerInput = analogRead(analogInPin);
-  int timeout = map(timerInput, 0, 1023, 10, 5000);
+  return map(timerInput, ANALOGLOW, ANALOGHIGH, 10, 5000);
+}
+
+/*
+  outputs inverted since pins will connect to LED ground
+  255 is therefore off and 0 is on
+*/
+void writeLeds(int pwmVal, float led1Weight, float led2Weight) {
+  analogWrite(pwmPin, PWMHIGH - (pwmVal * led1Weight));
+  analogWrite(pwmPin2, PWMHIGH - (pwmVal * led2Weight));
+}
+
+void loop() {
+  int timeout = readTimerInput();
 
   float multiplier1 = 0.1;
   float multiplier2 = 1.0 - multiplier1;
 
-  /*
-    outputs inverted since pins will connect to LED ground
-    255 is therefore off and 0 is on
-  */
-  if (stage == 0 && i <= 255) {
-    analogWrite(pwmPin, 255 - (i * multiplier1));
-    analogWrite(pwmPin2, 255 - (i * multiplier2));
+  if (fadingIn) {
+    writeLeds(i, multiplier1, multiplier2);
 
-    i++;
-    if (i == 255) {
-      stage = 1; // fade out
+    if (i == PWMHIGH) {
+      fadingIn = false;
+    }
+    else {
+      i++;
     }
 
     delayMicroseconds(timeout);
   }
-  else if (stage == 1 && i >= 0) {
-    analogWrite(pwmPin, 255 - (i * multiplier1));
-    analogWrite(pwmPin2, 255 - (i * multiplier2));
+  else {
+    writeLeds(i, multiplier1, multiplier2);
 
-    i--;
-    if (i == 0) {
-      stage = 0; // fade in
+    if (i == PWMLOW) {
+      fadingIn = true;
+    }
+    else {
+      i--;
     }
 
     delayMicroseconds(timeout);
