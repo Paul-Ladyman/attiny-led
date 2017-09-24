c95c429b47a192054a57106ac367258489ef802b
Paul-Ladyman <ladyman.paul@gmail.com>
2017-09-24 18:36:15 +0100

    Respond to analogue input changes faster
    - don't use for loops within loop function
    - delay in microseconds

--- attinytest.ino
+++ attinytest.ino
@@ -1,6 +1,8 @@
 int pwmPin = 0;
 int pwmPin2 = 1;
 int analogInPin = A1;
+int i = 0;
+int stage = 0; // fade in
 
 void setup()
 {
@@ -12,26 +14,35 @@ void setup()
 void loop()
 {
   int timerInput = analogRead(analogInPin);
-  int timeout = map(timerInput, 0, 1023, 1, 10);
-  
+  int timeout = map(timerInput, 0, 1023, 10, 5000);
+
   float multiplier1 = 0.1;
   float multiplier2 = 1.0 - multiplier1;
 
   /*
-     outputs inverted since pins will connect to LED ground
-     255 is therefore off and 0 is on
+    outputs inverted since pins will connect to LED ground
+    255 is therefore off and 0 is on
   */
-  for (int i = 0; i <= 255; i += 1)
-  {
+  if (stage == 0 && i <= 255) {
     analogWrite(pwmPin, 255 - (i * multiplier1));
     analogWrite(pwmPin2, 255 - (i * multiplier2));
-    delay(timeout);
-  }
 
-  for (int i = 255; i >= 0; i -= 1)
-  {
+    i++;
+    if (i == 255) {
+      stage = 1; // fade out
+    }
+
+    delayMicroseconds(timeout);
+  }
+  else if (stage == 1 && i >= 0) {
     analogWrite(pwmPin, 255 - (i * multiplier1));
     analogWrite(pwmPin2, 255 - (i * multiplier2));
-    delay(timeout);
+
+    i--;
+    if (i == 0) {
+      stage = 0; // fade in
+    }
+
+    delayMicroseconds(timeout);
   }
 }
